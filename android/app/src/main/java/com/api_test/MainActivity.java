99d037c37dd3a525b48339adb51bf1b92cdefe67
Alexander Murphy <amurphy345@gmail.com>
2018-02-03 00:27:53 -0700

    navigation working on both ios and android

--- android/app/src/main/java/com/api_test/MainActivity.java
+++ android/app/src/main/java/com/api_test/MainActivity.java
@@ -1,15 +1,17 @@
 package com.api_test;
 
-import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+// import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
+
+public class MainActivity extends SplashActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
-    protected String getMainComponentName() {
-        return "api_test";
-    }
+    // @Override
+     protected String getMainComponentName() {
+         return "api_test";
+     }
 }
---
+++ index.android.js
@@ -0,0 +1,11 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { Navigation } from 'react-native-navigation'
+
+Navigation.registerComponent('realmtest', () => App)
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'realmtest',
+    title: 'Welcome'
+  }
+})
