2dae1303d29e6b1c5409199363cd3ba9aa758fa6
Alexander Murphy <amurphy345@gmail.com>
2018-02-02 23:48:09 -0700

    adding vectors and navigation

--- App.js
+++ App.js
@@ -23,19 +23,19 @@ const instructions = Platform.select({
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      // <View style={styles.container}>
+      //   <Text style={styles.welcome}>
+      //     Welcome to React Native!
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     To get started, edit App.js
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     {instructions}
+      //   </Text>
+      // </View>
 
-      // <FlatListDemo/>
+      <FlatListDemo/>
 
     )
   }
--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { Navigation } from 'react-native-navigation'
 
-Navigation.registerComponent('realmtest', () => App);
+Navigation.registerComponent('realmtest', () => App)
 Navigation.startSingleScreenApp({
   screen: {
     screen: 'realmtest',
     title: 'Welcome'
   }
-});
+})
