d2710a4348d43506635faf321612ea964df8dcd9
Alexander Murphy <amurphy345@gmail.com>
2018-02-04 01:56:46 -0700

    working on card component

--- src/components/HorizontalScroller.js
+++ src/components/HorizontalScroller.js
@@ -46,11 +46,11 @@ const styles = StyleSheet.create({
         flex: 1,
         shadowColor: "black",
         shadowRadius: 14,
-        marginLeft: 12,
-        marginRight: 12, 
+        marginLeft: 6,
+        marginRight:6, 
         marginTop: 6,
         marginBottom: 12,
-        borderRadius: 20
+        borderRadius: 20,
     },
     symbol: {
         color: "black",
@@ -60,13 +60,29 @@ const styles = StyleSheet.create({
     infoStack: {
         marginTop: 24,
         marginLeft: 12,
-        flexDirection: "column"
+        flexDirection: "column",
+        backgroundColor: "transparent"
     },
     icon: {
         marginTop: 12,
         marginLeft: 12,
         height: 40,
         width: 40
+    },
+    cardContainer: {
+        overflow: "hidden",
+        // marginRight: 12
+        flex: 1,
+        margin: 0
+    },
+    largeIcon: {
+        position: "absolute",
+        marginTop: 30,
+        marginLeft: 70,
+        height: 100,
+        width: 100,
+        transform: [{ rotate: '320deg'}],
+        opacity: 0.7
     }
 })
 
@@ -79,12 +95,15 @@ class TitleCard extends Component {
         return (
             <View style={styles.card}>
                 <ElevatedView style={styles.elevated} elevation={6}>
-                    <FastImage resizeMode="contain" style={styles.icon} source={{uri:"https://bitcoin.org/img/icons/opengraph.png"}}/>
-                    <View style={styles.infoStack}>
-                        <Text style={styles.symbol}>BTC</Text>
-                        <Text>Bitcoin</Text>
-                        <Text>$6,000</Text>
-                    </View>   
+                    <View style={styles.cardContainer}>
+                        <FastImage Transforms resizeMode="contain" style={styles.largeIcon} source={{uri:"https://bitcoin.org/img/icons/opengraph.png"}}/>
+                        <FastImage resizeMode="contain" style={styles.icon} source={{uri:"https://bitcoin.org/img/icons/opengraph.png"}}/>          
+                        <View style={styles.infoStack}>
+                            <Text style={styles.symbol}>BTC</Text>
+                            <Text>Bitcoin</Text>
+                            <Text>$6,000</Text>
+                        </View>   
+                    </View>
                 </ElevatedView>
             </View>
         )
